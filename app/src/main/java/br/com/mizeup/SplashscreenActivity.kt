6645b17b52611c5c4e7fef322d84ff101d724003
Radrick Gomes <radrickgomes2708@hotmail.com>
2020-05-22 17:04:21 -0300

    correction of the splashscreen

--- app/src/main/java/br/com/mizeup/SplashscreenActivity.kt
+++ app/src/main/java/br/com/mizeup/SplashscreenActivity.kt
@@ -3,28 +3,27 @@ package br.com.mizeup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import kotlinx.android.synthetic.main.activity_splashscreen.*
+import android.os.Handler
 
-class SplashscreenActivity : AppCompatActivity(), View.OnClickListener {
+class SplashscreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
 
-        if (supportActionBar != null) {
-            supportActionBar!!.hide()
-        }
-
-        button_email_login.setOnClickListener(this)
-        button_other_login.setOnClickListener(this)
+        var handler = Handler()
+        handler(handler)
     }
 
-    override fun onClick(v: View) {
-        if (v.id == R.id.button_email_login) {
-            startActivity(Intent(this, MainActivity::class.java))
-        } else {
-            startActivity(Intent(this, MainActivity::class.java))
-        }
+    private fun handler(h: Handler) {
+        h.postDelayed(Runnable {
+            kotlin.run { initializeApp() }
+        }, 3000)
     }
+
+    private fun initializeApp(){
+        startActivity(Intent(this, LoginFormsActivity::class.java))
+        finish()
+    }
+
 }
